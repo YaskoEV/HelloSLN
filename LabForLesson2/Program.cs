56c8ba0d951730a93550e44cb1a3c63b4589bf7c
YaskoEV <84845994+YaskoEV@users.noreply.github.com>
2024-07-14 17:26:23 +0300

    lab_2
    Лабораторная по 2 модулю

--- LabForLesson2/Program.cs
+++ LabForLesson2/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace LabForLesson2
 {
-    internal class Program
+
+    enum Figures 
+    {
+        Romb = 1, 
+        Rectangle,
+        Cube
+    }
+
+    struct FigData
+    {
+        public int width;
+        public int height;
+        public Figures type;
+    }
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FigData rect = new FigData();
+            rect.type = Figures.Rectangle;
+            rect.width = 10;
+            rect.height = 5;
+
+            Console.WriteLine($"Площадь фигуры {rect.type} равна " + rect.width * rect.height + " см2");
         }
     }
 }
