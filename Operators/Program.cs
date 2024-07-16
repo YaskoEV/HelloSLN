bb1db5cf68eabbf33a3010073f4d7da1e8f96360
YaskoEV <84845994+YaskoEV@users.noreply.github.com>
2024-07-16 22:28:00 +0300

    continue... start lesson 2

---
+++ Operators/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = -55;
+            if (n > 0)
+            {
+                Console.WriteLine("n больше 0");
+                Console.WriteLine("n > 0");
+            }
+            else
+                if (n > -100) 
+                Console.WriteLine("n > -100");
+                else
+                Console.WriteLine("n Не больше 0");
+
+            int a = 5;
+            string s;
+            /*
+            if (a == 0)
+                s = "ноль";
+            else
+                s = "не ноль";
+            */
+
+            s = (a == 0) ? "ноль" : "не ноль";
+            Console.WriteLine(s);
+
+            string name = null;//"Sergey";
+            /*
+            if (name != null)
+                Console.WriteLine(name.ToUpper());
+            else
+                Console.WriteLine("Пустое имя");
+            */
+            Console.WriteLine(name != null ? name.ToUpper() : "пустое имя");
+            Console.WriteLine((name ?? "Пустое имя").ToUpper());
+        }
+    }
+}
