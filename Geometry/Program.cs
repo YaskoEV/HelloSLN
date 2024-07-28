bbe3ae747d3fdd28dd57e264529420533f6723af
YaskoEV <84845994+YaskoEV@users.noreply.github.com>
2024-07-28 15:34:56 +0300

    added decisions for mod4 ex 2
    Добавлено решение к модулю 4 задача 2-я

--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -14,8 +14,37 @@ namespace Geometry
 
     class Program
     {
+        static double area(Fdata fd)
+        {
+            double res = 0;
+            //if (fd.type.GetTypeCode() == 0) s = fd.a * fd.b / 2;
+            //switch (fd.type.ToString()) 
+            //{
+            //    case "Romb":  s = fd.a * fd.b / 2; break;
+            //    case "Rect": s = fd.a * fd.b; break;
+            //    case "RightTriangle": s = fd.a * fd.b / 2; break;
+            //    case "Circle": s = fd.a+ fd.a * Math.PI; break;
+            //    default: s = fd.a * fd.b / 2; break;
+            //};
+            res = (fd.type.ToString()) 
+                switch
+                {
+                    //"Romb" => s = fd.a * fd.b / 2,
+                    "Rect" => res = fd.a * fd.b,
+                    //"RightTriangle" => s = fd.a * fd.b / 2,
+                    "Circle" => res = fd.a + fd.a * Math.PI,
+                     _ => res = fd.a * fd.b / 2
+                };
+
+            return res;
+        }
         static void Main(string[] args)
         {
+            Romb romb = new Romb(new Fdata() { x0 = 1, y0 = 1, a = 4, b = 3});
+            Console.WriteLine(romb.GetType().Name);
+            Console.WriteLine(romb.Area());
+            Console.WriteLine("Ok");
+
             Rect rect = new Rect(new Fdata() {x0=1,y0=1,a=3,b=5});
             rect.Move(2, 1);
             rect.PrintInfo();
---
+++ mod4ex2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace mod4ex2
+{
+    internal class Program
+    {
+        static string IntToBinary(int x)
+        {
+            int count = 0;
+            int mask = 1;
+            int size = 32;
+
+            StringBuilder strX = new StringBuilder();
+            StringBuilder strXRes = new StringBuilder();
+
+            while (size > 0)
+            {
+                if (((mask) & x) > 0)
+                {
+                    strX = strX.Append("1");
+                }
+                else
+                {
+                    count++;
+                    strX = strX.Append("0");
+                }
+                mask = mask << 1;
+                size--;
+            }
+
+            for (int i = 31, j = 0; i >= 0; i--, j++)
+            {
+                //Console.Write(strX[i]);
+                strXRes.Append(strX[i]);
+                if (i == 8 || i == 16 || i == 24) 
+                { 
+                    //Console.Write(" ");
+                    strXRes.Append(" ");
+                }
+
+
+            }
+            //Console.WriteLine();
+
+            return strXRes.ToString();
+        }
+        static void Main(string[] args)
+        {
+            int x = 8;
+            Console.WriteLine(IntToBinary(x));
+        }
+    }
+}
