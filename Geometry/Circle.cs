d2a25e0f541a4462d4d0bc9e665ab13208334d3c
YaskoEV <84845994+YaskoEV@users.noreply.github.com>
2024-07-28 11:15:39 +0300

    add Geometry Prj
    Добавил проект Geometry для выполнения ДЗ к модулю 5

---
+++ Geometry/Circle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public class Circle
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        private double radius { get; set; }
+
+        public double checkR
+        {
+            get { return radius; }
+            set { if(value > 0)
+                    radius = value;
+                  else 
+                    throw new ArgumentException("Неверный радиус -  меньше или равен 0");
+                        
+                }
+        }
+
+        public Circle(int x, int y, double r) 
+        {
+            this.x = x;
+            this.y = y;
+            this.checkR = r;
+        }
+
+        //метод для вычисления площади круга
+        public double Area()
+        {
+            //Console.WriteLine("Радиус = {0}", checkR);
+            return Math.PI*Math.Pow(checkR, 2);
+        }
+
+        //метод для вычисления длины окружности
+        public double CircleLenght()
+        {
+            return 2*Math.PI*checkR;
+        }
+
+        //метод изменения радиуса окружности
+        public void Scale(double scale) 
+        {
+            checkR = checkR * scale;
+        }
+
+        public void Move(int x, int y) 
+        {
+            this.x += x;
+            this.y += y;
+            //Console.WriteLine("Внимание! Координаты фигуры изменились!\nНовые кооррдинаты центра: x = {0}, y = {1}", x, y);
+        }
+
+        public void PrintInfo()
+        { 
+            Console.WriteLine("Координаты центра окружности: x = {0}, y = {1}\nРадиус окружности = {2}", x, y, checkR); 
+        }
+    }
+}
---
+++ Geometry/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Geometry
+{
+    public enum Figures { Romb, Rect, RightTriangle, Circle }
+
+    public struct Fdata
+    {
+        public int x0, y0;
+        public double a, b;
+        public int color;
+        public Figures type;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rect rect = new Rect(new Fdata() {x0=1,y0=1,a=3,b=5});
+            rect.Move(2, 1);
+            rect.PrintInfo();
+            Console.WriteLine(rect.Area());
+
+            RightTriangle rt = new RightTriangle(new Fdata() { x0 = -1, y0 = -3, a = 1, b = 4 });
+            rt.Move(1, 1);
+            rt.PrintInfo();
+            Console.WriteLine(rt.Area());
+
+            Circle c = new Circle(0, 0, 10);
+            Console.WriteLine($"Площадь круга = {c.Area()}");
+            Console.WriteLine($"Длина окружности = {c.CircleLenght()}");
+            c.Scale(1.5);
+            Console.WriteLine($"Площадь круга = {c.Area()}");
+            Console.WriteLine($"Длина окружности = {c.CircleLenght()}");
+            c.Move(2, 1);
+            c.Move(-5, 8);
+            c.PrintInfo();
+
+
+            object[] data = {rect, rt};
+            Print(data);
+        }
+
+        public static void Print(object[] data)
+        {
+
+        }
+    }
+}
